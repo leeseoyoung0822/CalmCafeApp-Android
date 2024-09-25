0f4afae1899421dc147b7ed099267cbe57e65659
이서영 <seoyoung@iseoyeong-ui-MacBookAir.local>
2024-09-25 17:31:22 +0900

    nativekey

--- app/src/main/java/com/example/calmcafeapp/KakaoApplication.kt
+++ app/src/main/java/com/example/calmcafeapp/KakaoApplication.kt
@@ -1,4 +1,14 @@
 package com.example.calmcafeapp
 
-class KakaoApplication {
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class KakaoApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+
+        // Kakao SDK api 호출해서 초기화
+        KakaoSdk.init(this, "62ad8683c4c5d03977bde793e357956c")
+    }
 }
