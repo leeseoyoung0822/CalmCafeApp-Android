e4b3f5f63a4a3435040529cfeb85874f6b7b4ab5
Lee-EunSeok <lanmecan@naver.com>
2024-09-28 03:14:20 +0900

    navigation

--- app/src/main/java/com/example/calmcafeapp/MainActivity.kt
+++ app/src/main/java/com/example/calmcafeapp/MainActivity.kt
@@ -3,18 +3,77 @@ package com.example.calmcafeapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.calmcafeapp.databinding.ActivityMainBinding
+import com.example.calmcafeapp.ui.HomeFragment
+import com.example.calmcafeapp.ui.RankFragment
+import com.example.calmcafeapp.ui.SettingFragment
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showInit()
+        initBottomNav()
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+    }
+
+    private fun showInit() {
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.container_main, HomeFragment())
+        transaction.commit()
+    }
+
+    private fun initBottomNav() {
+        binding.navigationMain.itemIconTintList = null
+
+        binding.navigationMain.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.navigation_map -> {
+                    addFragment(HomeFragment())
+                }
+
+                R.id.navigation_rank -> {
+                    addFragment(RankFragment())
+                }
+
+                R.id.navigation_setting -> {
+                    addFragment(SettingFragment())
+                }
+            }
+            return@setOnItemSelectedListener true
         }
+
+        binding.navigationMain.setOnItemReselectedListener {  } // 바텀네비 재클릭시 화면 재생성 방지
+    }
+    fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container_main, fragment).commit()
+    }
+    fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .commit {
+                setCustomAnimations(
+                    R.anim.slide_3,
+                    R.anim.fade_out,
+                    R.anim.slide_1,
+                    R.anim.fade_out
+                )
+                replace(R.id.container_main, fragment)
+
+                addToBackStack(null)
+            }
     }
 }
---
+++ app/src/main/java/com/example/calmcafeapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.calmcafeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //스플래시 화면 표시 후 메인 액티비티로 이동 추후 JoinActivity로 변경
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
