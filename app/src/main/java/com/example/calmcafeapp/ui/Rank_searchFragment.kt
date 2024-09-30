3cad4c5584deddf2fe512ed882e4b715400d4a32
이서영 <seoyoung@iseoyeong-ui-MacBookAir.local>
2024-09-30 18:31:39 +0900

    recyclerview

--- app/src/main/java/com/example/calmcafeapp/ui/Rank_searchFragment.kt
+++ app/src/main/java/com/example/calmcafeapp/ui/Rank_searchFragment.kt
@@ -1,23 +1,16 @@
-package com.example.calmcafeapp.ui.rank
+package com.example.calmcafeapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.calmcafeapp.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Ranking_category1Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Ranking_category1Fragment : Fragment() {
+class Rank_searchFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -25,8 +18,8 @@ class Ranking_category1Fragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getString(com.example.calmcafeapp.ui.ARG_PARAM1)
+            param2 = it.getString(com.example.calmcafeapp.ui.ARG_PARAM2)
         }
     }
 
@@ -35,7 +28,7 @@ class Ranking_category1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ranking_category1, container, false)
+        return inflater.inflate(R.layout.fragment_ranking_searchview, container, false)
     }
 
     companion object {
@@ -45,15 +38,15 @@ class Ranking_category1Fragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Ranking_category1Fragment.
+         * @return A new instance of fragment Ranking_searchviewFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Ranking_category1Fragment().apply {
+            Rank_searchFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(com.example.calmcafeapp.ui.ARG_PARAM1, param1)
+                    putString(com.example.calmcafeapp.ui.ARG_PARAM2, param2)
                 }
             }
     }
