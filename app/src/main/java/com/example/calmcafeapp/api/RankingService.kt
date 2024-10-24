1e1824c1e61d660fa1a57dfe6ba2917b218a7bf0
이서영 <seoyoung@iseoyeong-ui-MacBookAir.local>
2024-10-24 17:11:30 +0900

    카카오에이피아이

---
+++ app/src/main/java/com/example/calmcafeapp/api/RankingService.kt
@@ -0,0 +1,4 @@
+package com.example.calmcafeapp.api
+
+interface RankingService {
+}
--- app/src/main/java/com/example/calmcafeapp/apiManager/ApiManager.kt
+++ app/src/main/java/com/example/calmcafeapp/apiManager/ApiManager.kt
@@ -1,6 +1,7 @@
 package com.example.calmcafeapp.apiManager
 
 import com.example.calmcafeapp.BuildConfig
+import com.example.calmcafeapp.api.LoginService
 import com.example.calmcafeapp.api.MapService
 import com.example.calmcafeapp.api.NaverReverseGeocodingService
 import com.example.calmcafeapp.api.ODsayService
@@ -22,11 +23,23 @@ import java.util.concurrent.TimeUnit
 
 object ApiManager {
 
+    // 회원 토큰 반환 api
+    private var BASE_URL = BuildConfig.AUTH_BASE_URL
+    val instance: LoginService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(LoginService::class.java)
+    }
+
+
     // 네이버 오픈 API Base URL
     private const val NAVER_OPEN_API_BASE_URL = "https://openapi.naver.com/"
     // 네이버 클라우드 플랫폼 API Base URL
     private const val NAVER_CLOUD_PLATFORM_API_BASE_URL = "https://naveropenapi.apigw.ntruss.com/"
-    private var BASE_URL = BuildConfig.AUTH_BASE_URL
+
     // 대중교통 API
     private const val ODSAY_BASE_URL = "https://api.odsay.com/v1/api/"
 
