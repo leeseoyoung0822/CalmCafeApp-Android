0ebbd1bd4b5512a5781485d786d8d916548c1aa7
이서영 <seoyoung@iseoyeong-ui-MacBookAir.local>
2024-11-23 21:48:29 +0900

    브랜치안바꿔서 커밋

--- app/src/main/java/com/example/calmcafeapp/data/FavoriteResponse.kt
+++ app/src/main/java/com/example/calmcafeapp/data/FavoriteResponse.kt
@@ -1,19 +1,36 @@
 package com.example.calmcafeapp.data
 
+import com.google.gson.annotations.SerializedName
+
 data class FavoriteResponse(
+    @SerializedName("isSuccess")
     val isSuccess: Boolean,
+
+    @SerializedName("code")
     val code: String,
+
+    @SerializedName("message")
     val message: String,
+
+    @SerializedName("result")
     val result: FavoriteResult
 )
 
 data class FavoriteResult(
+    @SerializedName("favoriteStoreDetailResDtoList")
     val favoriteStoreDetailResDtoList: List<FavoriteStore>
 )
 
 data class FavoriteStore(
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("storeCongestionLevel")
     val storeCongestionLevel: String,
+
+    @SerializedName("userCongestionLevel")
     val userCongestionLevel: String
 )
